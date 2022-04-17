54af13c36bb6c25e627817f1b59dfc4f471296bb
kamra <kamra@LAPTOP-M49P7DNF>
2022-04-17 17:48:49 +0400

    Task 3 finished

---
+++ task 3 17.04.2022/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace task_3_17._04._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eded elave edin");
+            int number = Convert.ToInt32(Console.ReadLine());
+            //int[] numbers;
+
+            if (number<50)
+            {
+                for (int i = 0; i < number; i++)
+                {
+                    if (i%3==0)
+                    {
+                        int numbersSize = (number / 3) + 1;
+                        int[] numbers = new int[numbersSize];
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+            if (number > 50 && number < 100)
+            {
+                for (int i = 0; i < number; i++)
+                {
+                    if (i % 5 == 0)
+                    {
+                        int numbersSize = (number / 5) + 1;
+                        int[] numbers = new int[numbersSize];
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+            if (number > 100)
+            {
+                for (int i = 0; i < number; i++)
+                {
+                    if (i % 8 == 0)
+                    {
+                        int numbersSize = (number / 8) + 1;
+                        int[] numbers = new int[numbersSize];
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+}
